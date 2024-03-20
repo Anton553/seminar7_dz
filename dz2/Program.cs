9fe8128fbd6e045b98010a1f45df1758278b032e
Anton <Anton@pochta.ru>
2024-03-20 21:55:53 +0300

    Добавил решение 2 задачи

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerm(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    else if (m >= 0 && n == 0)
+    {
+        return akkerm(m - 1, 1);
+    }
+    else
+    {
+        return akkerm(m - 1, akkerm(m, n - 1));
+    }
+}
+
+Console.WriteLine($"Ответ = {akkerm(m, n)}");
