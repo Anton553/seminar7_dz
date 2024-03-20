d323a536581aebfaadd85e0315c3e12d07b80f23
Anton <Anton@pochta.ru>
2024-03-20 21:40:28 +0300

    Добавил решение 1 задачи

--- Dz1/Program.cs
+++ Dz1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Вывод натуральных чисел от M до N: ");
+void NatChisMdoN(int m, int n)
+{
+    if (m > n)  return;
+    Console.Write($"{m} ");
+    NatChisMdoN(m + 1, n);
+}
+
+NatChisMdoN(m, n);
