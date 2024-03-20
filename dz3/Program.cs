74d4555ef3965d69032db0c0aaa976c2e4a02fff
Anton <Anton@pochta.ru>
2024-03-20 22:29:04 +0300

    Добавил решение 3 задачи

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = { 7, 6, 44, 3, 4 };
+
+
+void PrintElements(int[] array, int len)
+{
+    if (len > 0)
+    {
+        Console.Write(array[len] + " ");
+        PrintElements(array, len - 1);
+    }
+    else
+    {
+        Console.WriteLine(array[0]);
+    }
+
+}
+
+Console.WriteLine($"Выводим массив в обратном порядке: ");
+PrintElements(array, array.Length - 1);
